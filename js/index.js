08be660819df99809080121072bfe4c22b3f0726
Mark Viti <+>
2022-07-21 08:31:12 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
-document.addEventListener("DOMContentLoaded", function() {});
+URL = "http://localhost:3000/books"
+const listPanel = document.querySelector("#list-panel")
+const showPanel = document.querySelector("#show-panel")
+
+fetch(URL)
+.then(res => res.json())
+.then(displayAllBooks)
+
+function displayAllBooks(booksArray) {
+    booksArray.forEach(book => listBook(book))
+}
+
+function listBook(book){
+    const bookListing = document.createElement("li")
+
+    bookListing.innerText = book.title
+
+    bookListing.dataset.id = book.id
+
+    bookListing.addEventListener("click", event => {
+        showPanel.dataset.id = book.id
+
+        const showPanelId = showPanel.dataset.id
+
+        fetch(`${URL}/${showPanel.dataset.id}`)
+        .then(res => res.json())
+        .then(book => addUsers(book))
+
+
+        showPanel.innerHTML = `<image src = ${book.img_url}><h2>${book.title}</h2>
+        <h3>${book.author}</h3><p>${book.description}</p>`
+
+        function addUsers(bookObj) {
+            bookObj.users.forEach(user => {
+                listUser = document.createElement("li")
+                listUser.innerText = user.username
+                showPanel.appendChild(listUser)
+            })
+        }
+
+        const likeButton = document.createElement("button")
+        likeButton.innerText = "LIKE"
+        likeButton.setAttribute("id", "like-book")
+        showPanel.appendChild(likeButton)
+
+        likeButton.addEventListener("click", event => {
+            
+            if (likeButton.innerText == "LIKE") {
+                likeButton.innerText = "UNLIKE"
+                const yourUser = document.createElement("li")
+                yourUser.setAttribute("id", "your-user")
+                yourUser.innerText = "pouros"
+                showPanel.appendChild(yourUser)
+            } else {
+                likeButton.innerText = "LIKE"
+                const yourUser = document.querySelector('#your-user')
+                yourUser.remove()
+            }
+
+        
+        })
+
+    })
+
+    listPanel.appendChild(bookListing)
+}
